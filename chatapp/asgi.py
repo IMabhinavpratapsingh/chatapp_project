3dd4782cb0a6590377109efdf31e12870b4a0edd
IMabhinavpratapsingh <abhinavpratapsingh890cr@gmail.com>
2025-12-27 03:16:53 +0530

    configure django channels + render deployment

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -11,17 +11,20 @@ print("ASGI LOADED")
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import theapp.routing   # tumhara app jaha consumers hain
+from channels.auth import AuthMiddlewareStack
+
+import theapp.routing  # <-- ye important hai
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(theapp.routing.websocket_urlpatterns)
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(theapp.routing.websocket_urlpatterns)
+        ),
+    }
+)
